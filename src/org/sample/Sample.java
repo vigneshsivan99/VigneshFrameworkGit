701d6e02ade75c1c5c481292a9af686ef85e474f
unknown <vigneshmv1999@gmail.com>
2022-05-22 18:02:33 +0530

    added few lines

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -4,6 +4,7 @@ public class Sample {
 
         private void empId() {
 			System.out.println("empid");
+			System.out.println("rtyujhgfd");
 		}
         public static void main(String[] args) {
 			Sample s=new Sample();
