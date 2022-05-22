7d620d62ae39bf323229e5ebd30830e2e47e3c29
unknown <vigneshmv1999@gmail.com>
2022-05-22 17:55:11 +0530

    code added

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Sample {
+
+        private void empId() {
+			System.out.println("empid");
+		}
+        public static void main(String[] args) {
+			Sample s=new Sample();
+			s.empId();
+		}
+
+
+}
